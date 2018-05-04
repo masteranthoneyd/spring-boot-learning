a97fd70c36cc91eafca2a3d4c5bf3ac2c28237b4
yangbingdong <yangbingdong1994@gmail.com>
2018-05-04 19:12:05 +0800

    code optimize

---
+++ spring-boot-docker/src/main/java/com/yangbingdong/docker/pubsub/disruptor/handler/log/Say66666666Handler.java
@@ -0,0 +1,36 @@
+package com.yangbingdong.docker.pubsub.disruptor.handler.log;
+
+import com.yangbingdong.docker.pubsub.disruptor.core.DisruptorEventHandler;
+import com.yangbingdong.docker.pubsub.disruptor.event.log.AccessLogEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ybd
+ * @date 18-5-4
+ * @contact yangbingdong1994@gmail.com
+ */
+@Component
+@Slf4j
+public class Say66666666Handler implements DisruptorEventHandler<AccessLogEvent> {
+
+	@Override
+	public int order() {
+		return Integer.MIN_VALUE;
+	}
+
+	@Override
+	public void onEvent(AccessLogEvent event, long sequence, boolean endOfBatch, int currentShard) throws Exception {
+		log.info("6666666666666666666666666! sequence: {}, current shard: {}", sequence, currentShard);
+	}
+
+	@Override
+	public boolean enableSharding() {
+		return true;
+	}
+
+	@Override
+	public int shardingQuantity() {
+		return 2;
+	}
+}
